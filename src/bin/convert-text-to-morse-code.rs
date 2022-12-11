b13b47db8029618621e5e98cc807ef6808508a18
Nipun <shehanatuk@gmail.com>
2022-12-11 14:11:32 -0500

    added more to convert-text-to-morse-code.rs

--- src/bin/convert-text-to-morse-code.rs
+++ src/bin/convert-text-to-morse-code.rs
@@ -6,10 +6,6 @@ can be any type that has certain behavior.
 Traits are similar to interfaces in Java, although there are some differences
 */
 
-fn to_morse_code() -> Message {
-    
-}
-
 trait MorseCode {
     fn to_morse_code(&self) -> Message;
 }
@@ -32,6 +28,47 @@ impl std::fmt::Display for Pulse {
     }
 }
 
+impl MorseCode for String {
+    fn to_morse_code(&self) -> Message {
+        use Pulse::*;
+        let mut msg = Vec::with_capacity(self.len());
+        // Vec::with_capacity() creates a vactor with the given capacity but with zero length. Until this capacity is reached push() calls won't reallocate the vector.
+        for c in self.chars() {
+            let letter = match c {
+                'A' | 'a' => vec![Long, Short],
+                'B' | 'b' => vec![Long, Short, Short, Short],
+                'C' | 'c' => vec![Long, Short, Long, Short],
+                'D' | 'd' => vec![Long, Short, Short],
+                'E' | 'e' => vec![Short],
+                'F' | 'f' => vec![Short, Short, Long, Short],
+                'G' | 'g' => vec![Long, Long, Short],
+                'H' | 'h' => vec![Short, Short, Short, Short],
+                'I' | 'i' => vec![Short, Short],
+                'J' | 'j' => vec![Short, Long, Long, Long],
+                'K' | 'k' => vec![Long, Short, Long],
+                'L' | 'l' => vec![Short, Long, Short, Short],
+                'M' | 'm' => vec![Long, Long],
+                'N' | 'n' => vec![Long, Short],
+                'O' | 'o' => vec![Long, Long, Long],
+                'P' | 'p' => vec![Short, Long, Long, Short],
+                'Q' | 'q' => vec![Long, Long, Short, Long],
+                'R' | 'r' => vec![Short, Long, Short],
+                'S' | 's' => vec![Short, Short, Short],
+                'T' | 't' => vec![Long],
+                'U' | 'u' => vec![Short, Short, Long],
+                'V' | 'v' => vec![Short, Short, Short, Long],
+                'W' | 'w' => vec![Short, Long, Long],
+                'X' | 'x' => vec![Long, Short, Short, Long],
+                'Y' | 'y' => vec![Long, Short, Long, Long],
+                'Z' | 'z' => vec![Long, Long, Short, Short],
+                _ => continue,
+            };
+            msg.push(letter);
+        }
+      msg
+    }
+}
+
 fn print_morse_code(code: &Message) {
     for letter in code.iter() {
         for pulse in letter.iter() {
@@ -49,3 +86,33 @@ fn main() {
 
     print_morse_code(&greeting);
 }
+
+#[test]
+fn hello_world() {
+    use Pulse::*;
+
+    let expected = vec![
+        vec![Short, Short, Short, Short],
+        vec![Short],
+        vec![Short, Long, Short, Short],
+        vec![Short, Long, Short, Short],
+        vec![Long, Long, Long],
+        vec![Short, Long, Long],
+        vec![Long, Long, Long],
+        vec![Short, Long, Short],
+        vec![Short, Long, Short, Short],
+        vec![Long, Short, Short],
+    ];
+
+    let actual = "Hello, world".to_string().to_morse_code();
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn whole_alphabet() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890".to_string();
+
+    alphabet.to_morse_code();
+    alphabet.to_uppercase().to_morse_code();
+}
+
