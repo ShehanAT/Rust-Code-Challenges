167ca1b0fddeb2448647b2652d8bf34628e4535c
Nipun <shehanatuk@gmail.com>
2022-12-13 21:19:22 -0500

    added new file: has-deadline-reached.rs

---
+++ src/bin/has-deadline-reached.rs
@@ -0,0 +1,48 @@
+struct ImportantEvent {
+    // TODO: define data structure 
+}
+
+trait Deadline {
+    fn is_passed(&self) -> bool;
+}
+
+impl Deadline for ImportantEvent {
+    // TODO: implement trait 
+}
+
+fn main() {
+    let missed_christmas = ImportantEvent {
+        what: String::from("Christmas"),
+        when: Local.ymd(2020, 12, 25),
+    };
+
+    if missed_christmas.is_passed() {
+        println!("Oh well, maybe next year");
+    } else {
+        println!("Nice");
+    }
+}
+
+#[test]
+fn in_past() {
+    use chrono::Duration;
+
+    let event = ImportantEvent {
+        what: String::from("friend's birthday"),
+        when: Local::today() - Duration::hours(25),
+    };
+
+    assert!(event.is_passed())
+}
+
+#[test]
+fn in_future() {
+    use chrono::Duration;
+
+    let event = ImportantEvent {
+        what: String::from("friend's birthday"),
+        when: Local::today() + Duration::hours(25),
+    };
+
+    assert!(!event.is_passed())
+}
