88a690b174da51c537b7aad664811261cbb7f957
Nipun <shehanatuk@gmail.com>
2022-12-23 14:42:29 -0500

    added more to handle-inconsistent-dates.rs

--- src/bin/handle-inconsistent-dates.rs
+++ src/bin/handle-inconsistent-dates.rs
@@ -3,85 +3,116 @@ use chrono::format::ParseErrorKind::TooShort;
 use chrono::NaiveDate;
 use chrono::Duration;
 
-// Parses a string that represents a date. When a date
-// is unable to be determined, return `None`.
+fn is_year(field: &str) -> bool {
+    field.len() == 4 && field.chars().all(|x| x.is_ascii_digit())
+}
+
 fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
-    let parse_from_str = NaiveDate::parse_from_str;
+    let text = text.trim();
 
-    let format_1 = "%Y/%b/%d";
-    let format_2 = "%Y-%m-%d";
-    
-    let format_3 = "%d.%b.%Y";
-    let format_4 = "%b.%d.%Y";
+    // check that there are numbers 
+    if !text.bytes().any(|x| x.is_ascii_digit()) {
+        return None;
+    }
 
-    let format_1_fail = false;
-    let format_2_fail = false;
-    let format_3_fail = false;
-    let format_4_fail = false;
-    let mut parsing_status = String::new();
+    // Allow any known delimiter 
+   let fields: Vec<_> = text.split(['/', '-', ' ', '.'].as_slice()).collect(); // .as_slice() extracts a slice containing the entire vector
 
-    // for i in 1..5 {
-    //     let conversion_result = parse_from_str(text, &format!("format_{i}"));
-    //     println!("{:}", conversion_result.unwrap());
-    //     match conversion_result.unwrap() {
-    //         ParseError(Invalid) => return Err(ParseError(Invalid))
-    //     }
-    //     if(NaiveDate::default() != conversion_result.unwrap_or(NaiveDate::default())) {
-    //         // parsing process suceeded 
-    //         return conversion_result.unwrap().pred_opt()
-    //     }
-    // }
-    let default = NaiveDate::default();
-    let conversion_result_1 = parse_from_str(text, format_1).unwrap_or(default);
-    let conversion_result_2 = parse_from_str(text, format_2).unwrap_or(default);
-    let conversion_result_3 = parse_from_str(text, format_3).unwrap_or(default);
-    let conversion_result_4 = parse_from_str(text, format_4).unwrap_or(default);
+    let mut year = None;
+    let mut month = None;
+    let mut day = None;
 
-    let conversion_results_arr = [conversion_result_1, conversion_result_2, conversion_result_3, conversion_result_4];
+    for field in fields.iter() {
+        if field.len() < 3 { // skip any fields less than 3, which means day fields will be skipped
+            continue;
+        }
 
-    let failed_date = NaiveDate::from_ymd_opt(1970, 01, 01);
+        let m = match &field.to_lowercase()[..3] { // [..3] removes all characters past the third index position and returns chars [0-3]
+            "jan" => 1,
+            "feb" => 2, 
+            "mar" => 3,
+            "apr" => 4,
+            "may" => 5, 
+            "jun" => 6,
+            "jul" => 7,
+            "aug" => 8,
+            "sep" => 9,
+            "oct" => 10,
+            "nov" => 11,
+            "dec" => 12,
+            _ => continue,
+        };
 
-    // println!("{:}", conversion_result_1);
-    // println!("{:}", conversion_result_2);
-    // println!("{:}", conversion_result_3);
-    // println!("{:}", conversion_result_4);
-    // println!("{:}", conversion_result_5);
+        month = Some(m)
+    }
 
-    for i in conversion_results_arr.iter() {
-        println!("result: {:} duration: {:}", i.to_string(), *i - failed_date.unwrap());
-        if *i - failed_date.unwrap() != Duration::zero(){
-            return Some(*i)
+    for field in fields.iter() {
+        if is_year(field) {
+            year = field.parse::<i32>().ok(); // attempt to convert string type field into u32, will silently return Err() if not doable
+            continue;
+        }
+
+        if month.is_some() {
+            day = field.parse::<u32>().ok(); // Since month usual preceeds day, attempt to convert field to u2, assuming that its a valid day
+        } else {
+            month = field.parse::<u32>().ok(); // If only year and month is provided skip the day field 
         }
     }
 
-
-
-    // if(conversion_result_1 != ParseError(Invalid)) {
-    //     return converison_result_1;
-    // } else if (conversion_result_2 != ParseError(Invalid)) {
-    //     return converison_result_2;
-    // } else if (conversion_result_3 != ParseError(Invalid)) {
-    //     return converison_result_3;
-    // } else if (conversion_result_4 != ParseError(Invalid)) {
-    //     return converison_result_4;
-    // }
-
-    return None;
+    match (year, month, day) {
+        (Some(year), Some(month), Some(day)) => NaiveDate::from_ymd_opt(year, month, day),
+        (Some(year), Some(month), None) => NaiveDate::from_ymd_opt(year, month, 1),
+        _ => None
+    }
 }
+
+// By Sean A 
+// fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
+//     let parse_from_str = NaiveDate::parse_from_str;
+
+//     let format_1 = "%Y/%b/%d";
+//     let format_2 = "%Y-%m-%d";    
+//     let format_3 = "%d.%b.%Y";
+//     let format_4 = "%b.%d.%Y";
+//     let format_5 = "%Y %b %d";
+//     let format_6 = "%Y/%B/%d";
+
+
+//     let default = NaiveDate::default();
+//     let conversion_result_1 = parse_from_str(text, format_1).unwrap_or(default);
+//     let conversion_result_2 = parse_from_str(text, format_2).unwrap_or(default);
+//     let conversion_result_3 = parse_from_str(text, format_3).unwrap_or(default);
+//     let conversion_result_4 = parse_from_str(text, format_4).unwrap_or(default);
+//     let conversion_result_5 = parse_from_str(text, format_5).unwrap_or(default);
+//     let conversion_result_6 = parse_from_str(text, format_6).unwrap_or(default);
+
+//     let conversion_results_arr = [conversion_result_1, conversion_result_2, conversion_result_3, conversion_result_4, conversion_result_5, conversion_result_6];
+
+//     let failed_date = NaiveDate::from_ymd_opt(1970, 01, 01);
+
+//     for i in conversion_results_arr.iter() {
+//         // println!("result: {:} duration: {:}", i.to_string(), *i - failed_date.unwrap());
+//         if *i - failed_date.unwrap() != Duration::zero(){
+//             return Some(*i)
+//         }
+//     }
+
+//     return None;
+// }
 
 fn main() {
     let dates = [
-        "2010-12-11", 
-        "1999/Mar/02",
+        "2002 Feb 02",
+        "2010-12-11",
+        "1999/March/02",
         "01.Mar.2021",
         "Mar.05.2021",
         "not a date",
     ];
 
-    // for d in dates.iter() {
-    //     println!("{} -> {:?}", d, flexible_date_parse(d));
-    // }
-    println!("{} -> {:?}", dates[0], flexible_date_parse(dates[0]));
+    for d in dates.iter() {
+        println!("{} -> {:?}", d, flexible_date_parse(d));
+    }
 }
 
 #[test]
