1eeb1ab9537f5fc3cb0a5efd0b51e50bacffbc99
Nipun <shehanatuk@gmail.com>
2022-12-26 11:29:28 -0500

    added more to src/parser/vigenere-cipher.rs

--- src/bin/vigenere-cipher.rs
+++ src/bin/vigenere-cipher.rs
@@ -1,18 +1,12 @@
-use sycamore::prelude::*;
-
-
-
 mod vigenere {
     // *b: A string in Rust is a valid sequence of unicode characters and hence it can be represented as &[u8]. A byte is also an 8-bit integer so it is considered as a sequence of unicode bytes. 
     // Prefixing a string with 'b' converts the string to a sequence of bytes(of type u8). This is called a byte literal
     const SIZE: [u8; 26] = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const A: u8 = b'A';
     const Z: u8 = b'Z';
-    const WRAP: u8 = 26; 
+    const WRAP: u8 = 26;
 
-    use ciphers::{Vigenere, Cipher};
-
-    fn clean_input(plaintext: &str) -> impl Iterator<Item = u8> + '_{
+    fn clean_input(plaintext: &str) -> impl Iterator<Item = u8> + '_ {
         plaintext.bytes().filter_map(|x| 
             // x is a byte representation of a char, not char itself 
             match x {
@@ -25,7 +19,7 @@ mod vigenere {
     }
 
     pub fn encrypt(plaintext: &str, key: &str) -> String {
-        let mut key_bytes = key.bytes().map(|x| x - Z).cycle(); // The purpose for x - A seems to be for encryption purposes
+        let mut key_bytes = key.bytes().map(|x| x - A).cycle(); // The purpose for x - A seems to be for encryption purposes
 
         let encrypted_str = clean_input(plaintext)
             .map(|x| {
@@ -35,18 +29,18 @@ mod vigenere {
             .collect();
         
             // If you want to return an expression without the 'return' keyword then exclude the semi colon at its end
-            String::from_utf8(encrypted_str).unwrap()
+        String::from_utf8(encrypted_str).unwrap()
     }
 
     pub fn decrypt(ciphertext: &str, key: &str) -> String {
-        let mut key_bytes = key.bytes().map(|x| x - A).cycle(); // The purpose for x - A seems to be for decryption purposes
+        let mut key_bytes = key.bytes().map(|x| x - b'A').cycle(); // The purpose for x - A seems to be for decryption purposes
 
         let decrypted_str = clean_input(ciphertext)
-        .map(|x|{
-            let offset = key_bytes.next().unwrap();
-            ((x + WRAP - A) + offset) % WRAP + A
-        })
-        .collect();
+            .map(|x|{
+                let offset = key_bytes.next().unwrap();
+                ((x + WRAP - A) - offset) % WRAP + A
+            })
+            .collect();
 
         String::from_utf8(decrypted_str).unwrap()
     }
@@ -55,7 +49,13 @@ mod vigenere {
 fn main() {
 
     let key = "WHYRUST";
-    let encodemsg = "ENCODE_THIS";
+
+    // let ciphertext = "
+    // PVCDJG
+    // PAYCMY
+    // JRKUC
+    // ";
+
     let ciphertext = "
     PVCDJG
     PAYCMY
